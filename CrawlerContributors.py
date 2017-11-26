51eb72b746537bc8ad76100fa74dd9a58ea8b043
Leonardo W. Ávila <leonardo.avila98@outlook.com>
2017-11-26 20:51:51 -0200

    Update CrawlerContributors.py
    Reduzido um pouco mais o tamanho da amostra, em função de não ficar preso com o limite de requisições da API.

--- CrawlerContributors.py
+++ CrawlerContributors.py
@@ -4,7 +4,7 @@ import requests, csv, json
 #Limitando tanto os usuários quanto os repositórios, devido ao limite de requisições da API do GitHub.
 #h = Github()
 #repo = h.get_user("Microsoft").get_repo("vscode").contributors_url
-r = requests.get("https://api.github.com/repos/Microsoft/vscode/contributors?page=1&per_page=15")
+r = requests.get("https://api.github.com/repos/Microsoft/vscode/contributors?page=1&per_page=10")
 with open("crawlerContributorsVSCODE", 'w') as f:
     writeit = csv.writer(f, delimiter=',', lineterminator='\n')
     repoJSON = json.loads(r.text)
